30a7539938fe81f18ff10871390c80972c7af747
Arunkanakkillam <arungeo70@gmail.com>
2025-09-29 22:49:53 +0530

    socondday

--- postgis begin.sql
+++ postgis begin.sql
@@ -24,3 +24,35 @@ distanc_meters desc
 limit 3
 
 
+
+
+SELECT * FROM public.landmarks
+ORDER BY id ASC 
+select st_astext(geom) as land from landmarks
+select st_geometrytype(geom) from landmarks
+select st_dimension(geom) from landmarks limit 2
+select st_srid(geom) from landmarks
+
+create table line_only(
+id serial primary key,
+name text not null,
+category text,
+geom geometry(linestring,4326)
+);
+
+create table polygon_only(
+id serial primary key,
+name text not null,
+category text,
+geom geometry(polygon,4326)
+);
+
+select id,name,category,st_astext(geom) from line_only
+select id,name,category,st_astext(geom) from polygon_only
+
+select st_length (geom) as length from line_only
+select st_length(st_transform(geom,3857)) as length_meters from line_only
+select st_srid (geom) from line_only
+
+select st_area(geom) as area from polygon_only
+select st_area(st_transform (geom,32643)) as area_inM2 from polygon_only
